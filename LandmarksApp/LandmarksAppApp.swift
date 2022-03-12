ad595b46362cd1de547befdd6eae1684f52646ff
isaacshin92 <isaacshin92@gamil.com>
2022-03-12 21:49:57 +0900

    Initial Commit

---
+++ LandmarksApp/LandmarksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksAppApp.swift
+//  LandmarksApp
+//
+//  Created by Isaac Shin on 2022/03/12.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
